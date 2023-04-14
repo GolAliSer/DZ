518a7955df2df59103b7ed5933d6dc569cc45c05
GolAliSer <114601456+GolAliSer@users.noreply.github.com>
2023-04-14 21:37:39 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ package DZ;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.platform.commons.util.Match;
 
 public class Test {
     @Test
@@ -11,18 +12,21 @@ public class Test {
         int expected = 1;
         int actual = Match.countmatches(s1, s2);
         Assertions.assertEquals(expected, actual);
+        System.out.println("Expected = " + expected + " Actual = " +actual);
 
         s1 = "ASANOFLD"; //если вторая строка не входит в первую
         s2 = "PLO";
         expected = 0;
         actual = Match.countmatches(s1, s2);
         Assertions.assertEquals(expected, actual);
+        System.out.println("Expected = " + expected + " Actual = " +actual);
 
         s1 = "ASANOASAASA"; //если вторая строка входит в первую больше одного раза
         s2 = "ASA";
         expected = 3;
         actual = Match.countmatches(s1, s2);
         Assertions.assertEquals(expected, actual);
+        System.out.println("Expected = " + expected + " Actual = " +actual);
 
         //если одна из переменных пуста или обе пусты
         s1 = "";
@@ -30,17 +34,20 @@ public class Test {
         expected = 0;
         actual = Match.countmatches(s1, s2);
         Assertions.assertEquals(expected, actual);
+        System.out.println("Expected = " + expected + " Actual = " +actual);
 
         s1 = "ASANOASA"; //
         s2 = "";
         expected = 0;
         actual = Match.countmatches(s1, s2);
         Assertions.assertEquals(expected, actual);
+        System.out.println("Expected = " + expected + " Actual = " +actual);
 
         s1 = ""; //
         s2 = "";
         expected = 0;
         actual = Match.countmatches(s1, s2);
         Assertions.assertEquals(expected, actual);
+        System.out.println("Expected = " + expected + " Actual = " +actual);
     }
 }
